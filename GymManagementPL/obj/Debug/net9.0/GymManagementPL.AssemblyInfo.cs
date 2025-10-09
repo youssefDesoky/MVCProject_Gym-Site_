de08bbccf35361d9059df2151719b079852289cc
youssefDesoky <hgatvhh@gmail.com>
2025-10-09 23:58:30 +0300

    MVCSession03

--- GymManagementPL/obj/Debug/net9.0/GymManagementPL.AssemblyInfo.cs
+++ GymManagementPL/obj/Debug/net9.0/GymManagementPL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymManagementPL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d243a15fb2ade7e6dd10149180aa88dc029a689c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80eb7cc0df0fa5f3e352eea360a36cceeafcbe18")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymManagementPL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymManagementPL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
